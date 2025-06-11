eef2088d185fc1b075abdc01e85c71fb91e4e1ce
Alex <alexanderjamespollock@gmail.com>
2025-06-11 16:27:02 -0500

    Messed with stuff

--- src/utils/workout/workout_node.py
+++ src/utils/workout/workout_node.py
@@ -4,7 +4,7 @@ class workout_node:
     METERS_PER_MILE = 1600  # This apporximation is used to calculate mileage
     _slots_ = ("depth", "reps", "set", "pace", "unit")
 
-    def __init__(self, reps: int, set: str, pace: int, unit: bool):
+    def __init__(self, reps: int, set: str, pace: str, unit: bool):
         self.depth = 0
         self.reps = reps
         self.set = set
@@ -19,6 +19,7 @@ class workout_node:
             node.depth = self.depth+1
             self.children.append(node)
 
+    # A simple to string
     def __str__(self):
         toReturn = "   "*self.depth
         if (self.reps != None):
@@ -34,9 +35,14 @@ class workout_node:
             toReturn += str(child)
         return toReturn
 
+    # A way of calculating mileage based on the meters run in a workout
     def mileage(self):
-        return int(self.meterage())/METERS_PER_MILE
+        if not self.unit:
+            return int(self.meterage())/METERS_PER_MILE
+        else:
+            return self.set
 
+    # A way of calculating the meters run in a workout
     def meterage(self):
         multiplier = self.reps
         if (self.set != None):
@@ -48,13 +54,15 @@ class workout_node:
             return total
 
 
+# Test code (feel free to mess around/delete)
 test_workout = workout_node(4, None, None, False)
 times_two = workout_node(4, None, None, False)
-four_by_one = workout_node(4, 100, "7:30", False)  # 4x100 at 7:30
+four_by_one = workout_node(1, 100, "7:30", False)  # 4x100 at 7:30
 four_by_two = workout_node(4, 100, "8:00", False)  # 4x200 at 8:00
 test_workout.add(times_two)
 test_workout.add(times_two)
 times_two.add(four_by_one, four_by_two)
+print(test_workout)
 
-mile_workout = workout_node(None, 2, "ET", True)
+mile_workout = workout_node(1, 2, "ET", True)
 print(test_workout.mileage())
