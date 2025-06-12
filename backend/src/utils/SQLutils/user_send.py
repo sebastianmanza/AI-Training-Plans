837943af68b85d66f5b4a5950dd33d3b3d85ba24
connor heagy <connorheagy@gmail.com>
2025-06-12 16:59:15 -0500

    resolved conflicts in import packages

--- backend/src/utils/SQLutils/user_send.py
+++ backend/src/utils/SQLutils/user_send.py
@@ -1,17 +1,14 @@
 import psycopg2
-<<<<<<< HEAD
-import database_connect
-from utils.user_storage.user import user
-=======
 from database_connect import init_db
 from backend.src.utils.user_storage.user import user
->>>>>>> f9e23894502a5c59e4604794c05ae6cd6d901b33
 
 """Sends user information to the database."""
 def send_user_info(user_info, SQL_user):
     try:
         # Initialize the connection with the database
-        database = database_connect
+        database = database_connect()
+        
+        
         
 
         
