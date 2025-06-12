cc38464dcb9f3ba8f21e8bd0abe2413c8da4d4f3
connor heagy <connorheagy@gmail.com>
2025-06-12 16:53:24 -0500

    adding functions from database_connect

--- backend/src/utils/SQLutils/user_send.py
+++ backend/src/utils/SQLutils/user_send.py
@@ -1,12 +1,14 @@
 import psycopg2
-from utils.SQLutils import database_connect
+import database_connect
 from utils.user_storage.user import user
 
 """Sends user information to the database."""
-def send_user_info(user_info, SQL_host, SQL_password):
+def send_user_info(user_info, SQL_user):
     try:
         # Initialize the connection with the database
-        init_db(SQL_host, SQL_password)
+        database = database_connect
+        
+
         
         # Create a new user object
         user = user()
