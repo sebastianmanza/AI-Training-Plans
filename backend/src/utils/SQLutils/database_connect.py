84b87ae83aea33cf089090d2e1c44fe0904d1ba9
connor heagy <connorheagy@gmail.com>
2025-06-13 13:28:56 -0500

    removed class identification from database_connect.py

--- backend/src/utils/SQLutils/database_connect.py
+++ backend/src/utils/SQLutils/database_connect.py
@@ -1,49 +1,47 @@
 import psycopg2
+    
+# takes in the host name (localhost for database owner). 
+# Establish connection with the SQL database and return an error message if connection fails.
+def init_db(username, pwd):
+    try:
+        if (username != "postgres"):
+            locate = 'remote_host'
+        else:
+            locate = 'localhost'
+        # Establish connection
+        conn = psycopg2.connect(database = "UserListAi",
+                                user = username,
+                                host = locate,
+                                password = pwd,
+                                port = "5432")
+        return conn
+    except psycopg2.Error as e:
+        print(f"Database connection error: {e}")
+        return None
+    
+# Takes in a user ID and retreives their information from the SQL database. 
+def db_select(username, pwd, user_id):
+    
+    conn = init_db(username, pwd)
+    # open cursor to perform sql queries
+    curr = conn.cursor()
+    
+    # perpare the query 
+    database_query = (""" SELECT %s, dob, sex, runningex, fivekm, goaldate
+                            FROM public.userlistai; """)
+    # fill query with appropriate user ID
+    record_to_insert = (user_id)
+    
+    # execute query with filled parameters
+    curr.execute(database_query, record_to_insert)
+    # make changes in database persistent
+    conn.commit()
+    # close cursor
+    curr.close()
+        
 
-class database_connect:
-    
-    # takes in the host name (localhost for database owner). 
-    # Establish connection with the SQL database and return an error message if connection fails.
-    def init_db(username, pwd):
-        try:
-            if (username != "postgres"):
-                locate = 'remote_host'
-            else:
-                locate = 'localhost'
-            # Establish connection
-            conn = psycopg2.connect(database = "UserListAi",
-                                    user = username,
-                                    host = locate,
-                                    password = pwd,
-                                    port = "5432")
-            return conn
-        except psycopg2.Error as e:
-            print(f"Database connection error: {e}")
-            return None
-        
-    # Takes in a user ID and retreives their information from the SQL database. 
-    def db_select(username, pwd, user_id):
-        
-        conn = database_connect.init_db(username, pwd)
-        # open cursor to perform sql queries
-        curr = conn.cursor()
-        
-        # perpare the query 
-        database_query = (""" SELECT %s, dob, sex, runningex, fivekm, goaldate
-                                FROM public.userlistai; """)
-        # fill query with appropriate user ID
-        record_to_insert = (user_id)
-        
-        # execute query with filled parameters
-        curr.execute(database_query, record_to_insert)
-        # make changes in database persistent
-        conn.commit()
-        # close cursor
-        curr.close()
-            
-    
-    # takes in a user ID and updates given parameters
-    # def db_update(user_id, dob, sex, ):
+# takes in a user ID and updates given parameters
+# def db_update(user_id, dob, sex, ):
 
     
         
