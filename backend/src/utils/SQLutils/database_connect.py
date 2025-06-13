3c0a6a04d02c47fc34a36607d31fa51bd6111212
connor heagy <connorheagy@gmail.com>
2025-06-13 14:05:44 -0500

    added method to insert prelim datapoints into SQL database

--- backend/src/utils/SQLutils/database_connect.py
+++ backend/src/utils/SQLutils/database_connect.py
@@ -18,51 +18,6 @@ def init_db(username, pwd):
     except psycopg2.Error as e:
         print(f"Database connection error: {e}")
         return None
-    
-# Takes in a user ID and retreives their information from the SQL database. 
-def db_select(username, pwd, user_id):
-    
-    conn = init_db(username, pwd)
-    # open cursor to perform sql queries
-    curr = conn.cursor()
-    
-    # perpare the query 
-    database_query = (""" SELECT %s, dob, sex, runningex, fivekm, goaldate
-                            FROM public.userlistai; """)
-    # fill query with appropriate user ID
-    record_to_insert = (user_id)
-    
-    # execute query with filled parameters
-    curr.execute(database_query, record_to_insert)
-    # make changes in database persistent
-    conn.commit()
-    # close cursor
-    curr.close()
-        
-
-<<<<<<< HEAD
-# takes in a user ID and updates given parameters
-# def db_update(user_id, dob, sex, ):
-=======
-    # takes in the host name (localhost for database owner). 
-    # Establish connection with the SQL database and return an error message if connection fails.
-def init_db(username, pwd):
-    try:
-        if (username != "postgres"):
-            locate = 'remote_host'
-        else:
-            locate = 'localhost'
-        # Establish connection
-        conn = psycopg2.connect(database = "UserListAi",
-                                user = username,
-                                host = locate,
-                                password = pwd,
-                                port = "5432")
-        return conn
-    except psycopg2.Error as e:
-        print(f"Database connection error: {e}")
-        return None
-
 
         
 # Takes in a user ID and retreives their information from the SQL database. 
@@ -82,10 +37,23 @@ def db_select(username, pwd, user_id, query):
     # close cursor
     curr.close()
             
-    
-    # takes in a user ID and updates given parameters
-    # def db_update(user_id, dob, sex, ):
->>>>>>> d9651b62b76a2451f58d43568e7477c72c1db8da
+
+# Takes in prelim survey datapoints and inserts them into the SQL database
+def db_insert(username, pwd, query, user_id, dob, sex, runningex, fivekm, goaldate):
+        
+    conn = init_db(username, pwd)
+    # open cursor to perform sql queries
+    curr = conn.cursor()
+        
+    # fill query with appropriate user ID
+    record_to_insert = (user_id, dob, sex, runningex, fivekm, goaldate)
+        
+    # execute query with filled parameters
+    curr.execute(query, record_to_insert)
+    # make changes in database persistent
+    conn.commit()
+    # close cursor
+    curr.close()
 
     
         
