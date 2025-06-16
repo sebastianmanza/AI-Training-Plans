de7651a72f3ca99a8e0b6adbf068ff9258afae7f
connor heagy <connorheagy@gmail.com>
2025-06-16 13:48:06 -0500

    resovled minor conflict

--- backend/src/utils/SQLutils/user_send.py
+++ backend/src/utils/SQLutils/user_send.py
@@ -1,33 +1,52 @@
 import psycopg2
-from backend.src.utils.SQLutils.database_connect import db_insert
+from backend.src.utils.SQLutils.database_connect import db_insert, db_update
 from backend.src.utils.user_storage.user import user
-from backend.src.main.survey import main 
+from backend.src.utils.SQLutils.database_connect import init_db
+from backend.src.utils.SQLutils.config import DB_CREDENTIALS
 
 # Sends user information to the database.
-def send_user_info(new_user, SQL_user, password):
-    #prepare query 
-    db_query = """ INSERT INTO public.userlistai(dob, sex, runningex, fivekm, goaldate) VALUES (%s,%s,%s,%s,%s); """
+def send_user_info(new_user, username, password):
     
-    # for furture use
-    # param_count = db_query.count("%s")
-    # Initialize the connection with the database
-    db_insert(SQL_user, password, db_query, new_user.age, new_user.five_km_estimate, new_user.goal_date, new_user.running_ex,
+    try:
+        # Initialize the connection with the database
+        tryconn = init_db(username, password)
+        # open cursor to perform sql queries
+        curr = tryconn.cursor()
+        #check to see that the user does not already exist
+        db_query = """ SELECT * FROM public.userlistai WHERE userid = %s """
+        record_to_insert = (new_user.user_id)
+        
+         # execute query with filled parameters
+        curr.execute(db_query, record_to_insert)
+        # make changes in database persistent
+        tryconn.commit()
+        # close cursor
+        curr.close()
+        # close connection
+        tryconn.close()
+        
+        # now update the information of the existing user within the SQL database
+        db_update(username, password, new_user.userid, new_user.age, new_user.sex, new_user.five_km_estimate, new_user.goal_date, 
+                    new_user.running_ex, new_user.mean_RPE, new_user.STD_RPE)
+        
+    except Exception:
+        
+        # inserts as a new row in the database if user does not exist already
+        db_insert(username, password, new_user.age, new_user.sex, new_user.five_km_estimate, new_user.goal_date, new_user.running_ex,
                     new_user.mean_RPE, new_user.STD_RPE)
-    
-    
-    
-    send_user_info(main.prelim_survey(), "postgres", "Control1500#")
-    
-    
-    
-    
+  
+
+
+
+
+# testing     
+
+new_user = user("16", "F", "five", "4", 6, 8, 8)
+
     
 
-<<<<<<< HEAD
-    
-    
-=======
-    # Create a new user object
+send_user_info(new_user, DB_CREDENTIALS["DB_USERNAME"], DB_CREDENTIALS["DB_PASSWORD"])
 
-    user = user()
->>>>>>> 9f016e9e77f3080a85a8179aee0adf8342d69c4e
+
+# def send_month_info(user_id, username, password):
+    
