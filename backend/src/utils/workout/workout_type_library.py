bfd542ffcdfc09e1f68274f4b80aec287d831e82
owen1k2 <owenhblock@gmail.com>
2025-06-17 15:22:10 -0500

    updated to add off days and warmup and cooldown and updated variable terms for certain runs

--- backend/src/utils/workout/workout_type_library.py
+++ backend/src/utils/workout/workout_type_library.py
@@ -7,20 +7,22 @@ class workout_type_library:
     def create_trio(x, y, z):
         return (x, y, z)
     
-    ## x range is 1 - 7 , y range is 1 -10, z range is 1 - 10
+    ## x range is 1 - 7 stimulus, y range is 1 -10 RPE, z range is 1 - 10 Distance
     ## Dictionary that maps trios of (x, y, z) coordinates to workout types.
     workout_dictionary = {
         create_trio(2.5, 4, 5.5): "ET",
         create_trio(2, 3, 4.5): "Recovery",
-        create_trio(4, 6, 3): "Kenyan",
-        create_trio(2.5, 5, 9): "Long",
-        create_trio(4.5, 7, 4.5): "Threshold",
-        create_trio(5, 6, 4): "Fartlek",
-        create_trio(5.5, 8, 3.5): "Race Pace Interval",
+        create_trio(4, 6, 6): "Kenyan",
+        create_trio(2.5, 5, 10): "Long",
+        create_trio(4.5, 7, 7.5): "Threshold",
+        create_trio(5, 6, 7): "Fartlek",
+        create_trio(5.5, 8, 6.5): "Race Pace Interval",
         create_trio(6.5, 2, .5): "Strides",
         create_trio(7, 3, .5): "Hill Sprints",
         create_trio(7, 6, .5): "Flat Sprints",
-        create_trio(6, 10, 3): "Time Trial"
+        create_trio(6, 10, 3): "Time Trial",
+        create_trio(1, 2, 3): "Warmup and Cooldown",
+        create_trio(0, 0, 0,): "Off"
     }
 
     ## Returns a workout type based on the given x and y coordinates.
@@ -64,7 +66,6 @@ class workout_type_library:
         return real_difference
         
 # Example usage    
-print(workout_type_library.get_workout_type(7.3, 4, 1))  #Hill Sprints
-print(workout_type_library.get_workout_type_coordinates(7.3, 4, 1))  #ET (7, 3, .5)
-print(workout_type_library.get_workout_difference(7.3, 4, 1))  # (.3, 1, .5)
-
+print(workout_type_library.get_workout_type(2, 3, 6)) #Hill Sprints
+#print(workout_type_library.get_workout_type_coordinates(7.3, 4, 1))  #ET (7, 3, .5)
+#print(workout_type_library.get_workout_difference(7.3, 4, 1))  # (.3, 1, .5)
