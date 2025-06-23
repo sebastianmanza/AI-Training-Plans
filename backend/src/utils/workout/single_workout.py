87fb6624243aacea5ee9145da7d71cca340bccbc
owen1k2 <owenhblock@gmail.com>
2025-06-23 13:43:08 -0500

    created a new way to store workouts

--- backend/src/utils/workout/single_workout.py
+++ backend/src/utils/workout/single_workout.py
@@ -4,9 +4,9 @@ class single_workout:
 
     __slots__ = ("trio", "reps", "pace", "distance")
 
-    # trio = (x, y, z) = (stim, RPE, distance)
+    # trio = x, y, z = (stim, RPE, distance)
     # reps = list of ints (in meters)
-    # pace = list of strings (ss per rep)
+    # pace = list of strings (pace type: 5k, easy, mile, etc.)
     # distance = % of total long run distance (1-100)
     def __init__(self, trio, reps = list, pace = list, distance = int):
         self.trio = trio
