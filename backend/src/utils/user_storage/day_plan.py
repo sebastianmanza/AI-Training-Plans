f036d38c211559c94545f7d155f64d2bbf2cf05e
Sebastian <manzaseb@grinnell.edu>
2025-06-25 14:09:06 -0500

    bug fixes in user_retrieve

--- backend/src/utils/user_storage/day_plan.py
+++ backend/src/utils/user_storage/day_plan.py
@@ -7,13 +7,14 @@ class day_plan:
                  "lift", "expected_rpe", "real_rpe", "percent_completion", "workouts", "week_id")
 
     def __init__(self, workouts: list = [], total_mileage: int = -1, lift: bool = False, expected_rpe = -1, week_id: int = -1, 
-                 real_rpe: int = 0, completed_mileage: int = 0, percent_completion: int = 0, day_id = -1):
+                 real_rpe: int = 0, completed_mileage: int = 0, percent_completion: int = 0, day_id = -1, goal_stimuli = workout_database.create_trio(-1, -1, -1)):
         
         self.week_id = week_id  
         self.workouts = workouts
         self.total_mileage = total_mileage
         self.completed_mileage = completed_mileage
         self.percent_completion = percent_completion
+        self.goal_stimuli = goal_stimuli
 
         if len(workouts) < 1:
             self.goal_stimuli = workouts[0]
