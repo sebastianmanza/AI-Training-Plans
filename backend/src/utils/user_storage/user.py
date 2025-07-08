b831bca526aec21f0c68188ca2fd3895395c9518
Alex <alexanderjamespollock@gmail.com>
2025-07-08 15:58:21 -0500

    Added Mean RPE code

--- backend/src/utils/user_storage/user.py
+++ backend/src/utils/user_storage/user.py
@@ -9,6 +9,7 @@ import backend.src.utils.user_storage.training_database as training_database
 FIVEKDIST, METERS_PER_MILE = 5000, 1600  # Distance conversions
 CALCNUM = 1.06  # Exponent for pace prediction
 DISTANCES = [3000, 5000, 10000]  # Distances for which we will make predictions
+RPE, DAYS = 0, 1  # Indexes used later on
 
 
 class user:
@@ -42,6 +43,30 @@ class user:
         self.month_future = storage.month_future
         self.week_future = storage.week_future
         self.day_future = storage.day_future
+
+        # The key is the type of run. The first number in the value is the mean RPE
+        # and the second is the number of these workouts run. i.e.(5,2) implies the mean RPE is 5 after 2 workouts
+        self.workout_mean_RPE = {
+            "Easy Run": (0, 0),
+            "Recovery Run": (0, 0),
+            "Progression": (0, 0),
+            "Long Run": (0, 0),
+            "Threshold": (0, 0),
+            "Fartlek": (0, 0),
+            "Race Pace Interval": (0, 0),
+            "Strides": (0, 0),
+            "Hill Sprints": (0, 0),
+            "Flat Sprints": (0, 0),
+            "Time Trial": (0, 0),
+            "Warmup and Cooldown": (0, 0),
+            "Off": (0, 0)
+        }
+
+    # Update the mean_RPE using the workout type and the RPE
+    def update_mean_RPE(self, type: str, workout_RPE: float) -> None:
+        info = self.workout_mean_RPE.get(type) # Get the info for the workout type
+        new_mean = ((info[RPE]*info[DAYS]) + workout_RPE) / (info[DAYS]+1) # Calculate the new mean
+        self.workout_mean_RPE.update(type, (new_mean, (info[DAYS]+1))) # Update the information
 
     # Takes in a distance and assigns the mile pace to it.
     def set_pace(self, distance: int, new_pace) -> None:
