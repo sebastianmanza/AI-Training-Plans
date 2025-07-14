d8e2594efa5f8b008c8408f0395ba5d34776975e
connor heagy <connorheagy@gmail.com>
2025-07-14 13:59:37 -0500

    fixing user_send and user_retrieve pt. 2

--- backend/src/main/test_user_cycle.py
+++ backend/src/main/test_user_cycle.py
@@ -6,27 +6,28 @@ current_dir = os.path.dirname(__file__)
 while not os.path.exists(os.path.join(current_dir, "backend")):
     current_dir = os.path.dirname(current_dir)
     if current_dir == "/":  # Stop if we reach the root of the filesystem
-        raise RuntimeError("Could not find 'backend' folder in the directory hierarchy.")
+        raise RuntimeError(
+            "Could not find 'backend' folder in the directory hierarchy.")
 
 # Add the root directory to the Python path
 sys.path.append(current_dir)
-
-
-from backend.src.utils.user_storage.user import user
-from backend.src.utils.user_storage.training_database import training_database
-from backend.src.utils.SQLutils.user_send import send_user_all
-from backend.src.utils.SQLutils.user_retrieve import populate_user_info
-from backend.src.main.survey import main
-from backend.scripts.txt_to_database import txt_to_database
 from backend.src.utils.SQLutils.config import DB_CREDENTIALS
-
+from backend.scripts.txt_to_database import txt_to_database
+from backend.src.main.survey import main
+from backend.src.utils.SQLutils.user_retrieve import populate_user_info
+from backend.src.utils.SQLutils.user_send import send_user_all
+from backend.src.utils.user_storage.training_database import training_database
+from backend.src.utils.user_storage.user import user
 
 
 database = txt_to_database("backend/data/raw/training_plan_test.txt")
 # print(database._instance_)
+my_dict = [12, 34, 45]
+available_days = ["Mon", "Fri", "Sat"]
+list_of_workouts = [1.1, 2.2, 3.3]
+login_info = ["conballs@gmail.com", "concon", "secure_password"]
 
-
-test_user = user(12345, "male", "advanced", "17:45", 4, 5, 2)
+test_user = user("Aug", "M", "Advanced", 1, 2, 12, "Nov", my_dict, available_days, 7, 100, list_of_workouts)
 test_user.day_future = database.day
 test_user.week_future = database.week
 test_user.month_future = database.month
@@ -36,25 +37,21 @@ def test_user_cycle():
     """
     Test the user cycle functionality.
     """
-    
-    #test_user = main.prelim_survey()
-    send_user_all(test_user, DB_CREDENTIALS["DB_USERNAME"], DB_CREDENTIALS["DB_PASSWORD"])
-    
+# test_user = main.prelim_survey()
+    send_user_all(test_user, DB_CREDENTIALS["DB_USERNAME"], DB_CREDENTIALS["DB_PASSWORD"], login_info)
+
     return_user = populate_user_info(test_user.user_id)
-    print("User information retrieved")
-    
-    #print(database.day.get().expected_rpe)
-    # print("Day future expected RPE")
-    # print(test_user.day_future.get().expected_rpe)
-    # print("working")
-    # return 
-    # print(test_user.user_id)
+# #     print("User information retrieved")
+
+# #     # print(database.day.get().expected_rpe)
+# #     # print("Day future expected RPE")
+# #     # print(test_user.day_future.get().expected_rpe)
+# #     # print("working")
+# #     # return
+    print(test_user.user_id)
     print(return_user.user_id)
-    print(return_user.age)
-    print(return_user.sex)
     
-    print(return_user.day_future.get().workouts)
+    print(return_user.sex)
+#     print(return_user.day_future.get().workouts)
 
 test_user_cycle()
-    
-    
--- backend/src/utils/user_storage/user.py
+++ backend/src/utils/user_storage/user.py
@@ -5,84 +5,125 @@ import datetime
 from backend.src.utils.user_storage.storage_stacks_and_queues import storage_stacks_and_queues
 import backend.src.utils.time_conversion as tc
 import backend.src.utils.user_storage.training_database as training_database
+#from backend.src.utils.pace_calculations import get_training_pace_helper
 
 FIVEKDIST, METERS_PER_MILE = 5000, 1600  # Distance conversions
 CALCNUM = 1.06  # Exponent for pace prediction
 DISTANCES = [3000, 5000, 10000]  # Distances for which we will make predictions
+RPE, DAYS, DEVIATION = 0, 1, 2  # Indexes used for mean RPE
+DEFAULT_WORKOUT_NUMS = {
+    "Easy Run": (0, 0, 0), "Recovery Run": (0, 0, 0), "Progression": (0, 0, 0), "Long Run": (0, 0, 0),
+    "Threshold": (0, 0, 0), "Fartlek": (0, 0, 0), "Race Pace Interval": (0, 0, 0), "Strides": (0, 0, 0),
+    "Hill Sprints": (0, 0, 0), "Flat Sprints": (0, 0, 0), "Time Trial": (0, 0, 0), "Warmup and Cooldown": (0, 0, 0), "Off": (0, 0, 0)}
 
 
 class user:
-    # __slots__ = ("age", "dob", "sex", "five_km_estimate", "when-to-run", "injury", "mileage", "wo_history", "goal_date")
+    # __slots__ = ("dob", "sex", "running_ex", "injury", "most_recent_injury", "longest_run", "goal_date", "pace_estimates", "available_days", "number_of_days", "user_id", "workout_RPE")
 
-    def __init__(self, dob, sex, running_ex, five_km_estimate, goal_date, mean_RPE, STD_RPE, user_id=secrets.randbelow(100000000 - 10000000), longest_run=0):
+    def __init__(self, dob: str, sex: str, running_ex: str, injury: int, most_recent_injury: int, longest_run: int,  goal_date: str, 
+                pace_estimates: list, available_days: list, number_of_days: int, user_id: int = secrets.randbelow(100000000 - 10000000), workout_RPE= list):
+        """Creates a user from the given arguments and initializes storage which is the series of stacks and queues necessary for 
+        storing all past and future workouts from a training plan for the user.
+        Args:
+            self: The user
+            dob: --string: The users date of birth
+            sex: --string: The users sex
+            running_ex: --string: The users running level
+            injury: --int: How many injuries a user has had in the last 2 years
+            most_recent_injury: --int: The number of months ago the users most recent injury occured
+            longest_run: --int: The users longest average weekly long run
+            goal_date: --string: The date of the users most important race
+            pace_estimates: --dict: The expected pace of different run types for the user
+            available_dats: --list: The days of the week the user can run in the form of:
+                                    0 - unavailable
+                                    1 - available
+                                    2 - long run
+            number_of_days: --int: The number of days a user wants to run per week
+            user_id: --int The user's id
+            workout_RPE: --dict: Users mean RPE for each type of run
+        """
+        #training storage
         storage = storage_stacks_and_queues()
-        self.user_id = user_id
+        #inputs
         self.dob = dob
-        self.age = self.get_age()
         self.sex = sex
-
-        self.when_to_run = None
-        self.injury = None
-        self.goal_date = goal_date
-
-        self.longest_run = longest_run
         self.running_ex = running_ex
-        self.times = {}
-        self.five_km_estimate_seconds = tc.mile_pace(
-            tc.from_str(five_km_estimate), FIVEKDIST)
-        self.set_pace(FIVEKDIST, self.five_km_estimate_seconds)
-        self.make_predictions()
-
-        self.mean_RPE = mean_RPE
-        self.STD_RPE = STD_RPE
-
+        self.injury = injury
+        self.most_recent_injury = most_recent_injury
+        self.longest_run = longest_run
+        self.goal_date = goal_date
+        self.pace_estimates = pace_estimates
+        self.available_days = available_days
+        self.number_of_days = number_of_days
+        self.user_id = user_id
+        self.workout_RPE = workout_RPE
+        #workout storage
         self.month_history = storage.month_history
         self.week_history = storage.week_history
         self.day_history = storage.day_history
         self.month_future = storage.month_future
         self.week_future = storage.week_future
         self.day_future = storage.day_future
+        #additional information
+        #self.age = self.get_age()
+
+        
+
+    # Update the mean_RPE using the workout type and the RPE
+
+    def update_mean_RPE(self, type: str, given_RPE: float, expected_RPE: float) -> None:
+        # Get the info for the workout type
+        info = self.workout_mean_RPE.get(type)
+        new_mean = ((info[RPE]*info[DAYS]) + given_RPE) / \
+            (info[DAYS]+1)  # Calculate the new mean
+        # Calculate the new average deviation
+        new_deviation = ((info[DEVIATION]*info[DAYS]) +
+                         abs(given_RPE-expected_RPE)) / (info[DAYS]+1)
+        self.workout_mean_RPE.update(
+            type, (new_mean, (info[DAYS]+1)))  # Update the information
 
     # Takes in a distance and assigns the mile pace to it.
-    def set_pace(self, distance: int, new_pace):
+
+    def set_pace(self, distance: int, new_pace) -> None:
         if isinstance(new_pace, str):
-            self.times[distance] = tc.mile_pace(new_pace, distance)
+            self.pace_times_dict[distance] = tc.mile_pace(new_pace, distance)
         else:
-            self.times[distance] = new_pace
+            self.pace_times_dict[distance] = new_pace
 
     # Returns the mile pace for a given distance in seconds.
-    def get_pace(self, distance: int):
-        return self.times[distance]
+    def get_pace(self, distance: int) -> int:
+        return self.pace_times_dict[distance]
 
     # Makes the predictions for every distance in DISTANCES.
-    def make_predictions(self):
+    def make_predictions(self) -> None:
         for distance in DISTANCES:
             self.set_pace(distance, self.predict_pace(distance))
 
     # Predicts the mile pace for a given distance based on the 5k pace.
-    def predict_pace(self, distance):
+    def predict_pace(self, distance) -> int:
         fivekpace = self.get_pace(FIVEKDIST)
         return math.floor((fivekpace)*pow((distance/FIVEKDIST), CALCNUM)*(FIVEKDIST / distance))
 
     # Returns the mile pace for each distance.
-    def get_times(self):
+    def get_times(self) -> str:
         toReturn = ""
-        for k, v in self.times.items():
+        for k, v in self.pace_times_dict.items():
             toReturn += f"{k}:{tc.to_str(v)}\n"
         return toReturn
 
-    def get_user_id(self):
+    def get_user_id(self) -> int:
         return self.user_id
 
-    def generate_new_id(self):
+    def generate_new_id(self) -> None:
         self.user_id = secrets.randbelow(100000000 - 10000000)
 
-    def get_age(self):
-        today = datetime.date.today()
-        dob = datetime.datetime.strptime(self.dob, "%m/%d/%Y").date()
-        age = today.year - dob.year - \
-            ((today.month, today.day) < (dob.month, dob.day))
-        return age
+    # def get_age(self) -> int:
+    #     """Returns the number of years the user has been alive as an int"""
+    #     today = datetime.date.today()
+    #     dob = datetime.datetime.strptime(self.dob, "%m/%d/%Y").date()
+    #     age = today.year - dob.year - \
+    #         ((today.month, today.day) < (dob.month, dob.day))
+    #     return age
 
     # update training
 
@@ -120,23 +161,45 @@ class user:
 
         # Takes in a string and a user i.e. (5000+10, 17:30 5k runner) and returns the pace associated with it.
 
-    def parse_pace(self, pace: str):
-        if pace.find("+") != -1:
+    def parse_pace(self, pace: str) -> int:
+        if pace.find("+") != -1:  # See if a value is being added
             distance, increase = pace.split("+")
             increase = int(increase)
-        elif pace.find("-") != -1:
+        elif pace.find("-") != -1:  # See if a value is being subtracted
             distance, increase = pace.split("-")
             increase = -int(increase)
-        else:
-            increase = 0
+        else:  # No increase
+            distance, increase = pace, 0
         pace = pace.strip()
         seconds = self.get_pace(int(distance))
         return tc.alter_pace(seconds, increase)
 
+    def get_training_pace(self, type) -> int:
+        """Returns the training pace for a given type of workout."""
+        if type == "Easy Run":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.67)
+        elif type == "Progression":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.82)
+        elif type == "Recovery Run":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.64)
+        elif type == "Threshold":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.87)
+        elif type == "Long Run":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.7)
+        elif type == "VO2Max":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.95)
+        elif type == "Tempo":
+            return get_training_pace_helper(5000, self.five_km_estimate_seconds * 3.1, 0.82)
+        else:
+            return 0
 
-alex = user("8/22/2005", "male", "advanced", "17:30", "5", "7", "1")
-print(alex.get_pace(10000))
-print(alex.parse_pace("10000+10"))
+# alex = user("8/22/2005", "male", "advanced", "17:30", "5", "7", "1")
+# # print(alex.get_pace(10000))
+# # print(alex.parse_pace("10000"))
+
+# print(alex.five_km_estimate_seconds)
+# print(alex.get_training_pace("Recovery Run"))
+
 # alex.set_pace(5000, "17:30")
 # alex.make_predictions()
 # print(alex.get_times())
