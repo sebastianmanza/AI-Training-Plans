30d45c7492a862b346de0815264d6805152c8b8f
Alex <alexanderjamespollock@gmail.com>
2025-07-14 11:01:31 -0500

    Talk to me about this ~Alex

--- backend/src/utils/workout/workout_database.py
+++ backend/src/utils/workout/workout_database.py
@@ -45,102 +45,67 @@ class workout_database:
         self.long = workout_database.storage.long
         self.threshold = workout_database.storage.threshold
         self.fartlek = workout_database.storage.fartlek
-        self.race_pace_interval = workout_database.storage
+        self.race_pace_interval = workout_database.storage.race_pace_interval
         self.strides = workout_database.storage.strides
         self.hill_sprints = workout_database.storage.hill_sprints
         self.flat_sprints = workout_database.storage.flat_sprints
         self.time_trial = workout_database.storage.time_trial
         self.warmup_and_cooldown = workout_database.storage.warmup_and_cooldown
 
+    # This function is finicky but a god send. Make sure to reference it when you would write if/else statements
+    def match_execute(self, workout_type: str, func, *args):
+        """Execute a function on all workouts of a specific type"""
+        match workout_type:
+            case "Easy Run":
+                return func(self.et, *args)
+            case "Recovery Run":
+                return func(self.recovery, *args)
+            case "Progression":
+                return func(self.kenyan, *args)
+            case "Long Run":
+                return func(self.long, *args)
+            case "Threshold":
+                return func(self.threshold, *args)
+            case "Fartlek":
+                return func(self.fartlek, *args)
+            case "Race Pace Interval":
+                return func(self.race_pace_interval, *args)
+            case "Strides":
+                return func(self.strides, *args)
+            case "Hill Sprints":
+                return func(self.hill_sprints, *args)
+            case "Flat Sprints":
+                return func(self.flat_sprints, *args)
+            case "Time Trial":
+                return func(self.time_trial, *args)
+            case _:  # Default case for Warmup and Cooldown
+                return func(self.warmup_and_cooldown, *args)
+
     def add_workout(self, workout: single_workout) -> None:
         """Add a workout to the matching workout database"""
+        if not isinstance(workout, single_workout):
+            raise TypeError("Input must be a single_workout instance")
         workout_type = workout_database.get_workout_type(
             workout.trio[TRIO_STIM], workout.trio[TRIO_RPE], workout.trio[TRIO_DIST])
-        if workout_type == "Easy Run":
-            workout_database.storage.et.append(workout)
-        elif workout_type == "Recovery Run":
-            workout_database.storage.recovery.append(workout)
-        elif workout_type == "Progression":
-            workout_database.storage.kenyan.append(workout)
-        elif workout_type == "Long Run":
-            workout_database.storage.long.append(workout)
-        elif workout_type == "Threshold":
-            workout_database.storage.threshold.append(workout)
-        elif workout_type == "Fartlek":
-            workout_database.storage.fartlek.append(workout)
-        elif workout_type == "Race Pace Interval":
-            workout_database.storage.race_pace_interval.append(workout)
-        elif workout_type == "Strides":
-            workout_database.storage.strides.append(workout)
-        elif workout_type == "Hill Sprints":
-            workout_database.storage.hill_sprints.append(workout)
-        elif workout_type == "Flat Sprints":
-            workout_database.storage.flat_sprints.append(workout)
-        elif workout_type == "Time Trial":
-            workout_database.storage.time_trial.append(workout)
-        else:
-            workout_database.storage.warmup_and_cooldown.append(workout)
+        self.match_execute(workout_type, list.append, workout)
 
     def mass_add_workouts(self, workouts) -> None:
         """"Add a list of workouts to the database"""
         for workout in workouts:
-            if not (isinstance(workout, single_workout)):
-                raise (
-                    "Tried to add something that isn't a workout to the workout database")
-            else:
+            try:
                 self.add_workout(workout)
+            except TypeError:  # If the workout isn't a single_workout instance, raise an error
+                print(TypeError)
+
+    def print_all_type(self, type_list: list, workout_type: str) -> None:
+        """Print out all workouts of a specific type"""
+        print(f"{workout_type} Workouts:")
+        for workout in type_list:
+            print(workout)
 
     def print_workouts(self, workout_type: str) -> None:
         """Print out every workout of an inputted type"""
-        if workout_type == "Easy Run":
-            print("ET Workouts:")
-            for workout in self.et:
-                print(workout)
-        elif workout_type == "Recovery Run":
-            print("Recovery Workouts:")
-            for workout in self.recovery:
-                print(workout)
-        elif workout_type == "Progression":
-            print("Kenyan Workouts:")
-            for workout in self.kenyan:
-                print(workout)
-        elif workout_type == "Long Run":
-            print("Long Workouts:")
-            for workout in self.long:
-                print(workout)
-        elif workout_type == "Threshold":
-            print("Threshold Workouts:")
-            for workout in self.threshold:
-                print(workout)
-        elif workout_type == "Fartlek":
-            print("Fartlek Workouts:")
-            for workout in self.fartlek:
-                print(workout)
-        elif workout_type == "Race Pace Interval":
-            print("Race Pace Interval Workouts:")
-            for workout in self.race_pace_interval:
-                print(workout)
-        elif workout_type == "Strides":
-            print("Strides Workouts:")
-            for workout in self.strides:
-                print(workout)
-        elif workout_type == "Hill Sprints":
-            print("Hill Sprints Workouts:")
-            for workout in self.hill_sprints:
-                print(workout)
-        elif workout_type == "Flat Sprints":
-            print("Flat Sprints Workouts:")
-            for workout in self.flat_sprints:
-                print(workout)
-        elif workout_type == "Time Trial":
-            print("Time Trial Workouts:")
-            for workout in self.time_trial:
-                print(workout)
-        else:
-            workout_type == "Warmup and Cooldown"
-            print("Warmup and Cooldown Workouts:")
-            for workout in self.warmup_and_cooldown:
-                print(workout)
+        self.match_execute(workout_type, self.print_all_type, workout_type)
 
     # Returns a workout type based on the given x,y and z coordinates represented as stim, rpe and dist.
     @staticmethod
@@ -168,30 +133,8 @@ class workout_database:
         return workout_database.get_workout_type(stim, rpe, dist)
 
     def get_workout_storage_type(self, workout_type: str) -> list:
-        if workout_type == "Easy Run":
-            return workout_database.storage.et
-        elif workout_type == "Recovery Run":
-            return workout_database.storage.recovery
-        elif workout_type == "Progression":
-            return workout_database.storage.kenyan
-        elif workout_type == "Long Run":
-            return workout_database.storage.long
-        elif workout_type == "Threshold":
-            return workout_database.storage.threshold
-        elif workout_type == "Fartlek":
-            return workout_database.storage.fartlek
-        elif workout_type == "Race Pace Interval":
-            return workout_database.storage
-        elif workout_type == "Strides":
-            return workout_database.storage.strides
-        elif workout_type == "Hill Sprints":
-            return workout_database.storage.hill_sprints
-        elif workout_type == "Flat Sprints":
-            return workout_database.storage.flat_sprints
-        elif workout_type == "Time Trial":
-            return workout_database.storage.time_trial
-        else:
-            return workout_database.storage.warmup_and_cooldown
+        """Returns the list of workouts of a specific type"""
+        return self.match_execute(workout_type, lambda lst: lst)
 
     def get_individual_workout_helper(self, stim: float, rpe: float, dist: float, workout_type: str) -> single_workout:
         """Returns the workout closest to the stim,rpe and dist from within the type"""
