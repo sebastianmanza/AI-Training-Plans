7af69c99de21a7a604e2703d92ee3046efac4140
owen1k2 <owenhblock@gmail.com>
2025-07-23 13:15:30 -0500

    workouts are now functional with float inputs in the trio

--- backend/tests/workout_test.py
+++ backend/tests/workout_test.py
@@ -8,7 +8,7 @@ wd = workout_database()
 def test_workout_database():
     workouts = get_workout_list()
     wd.mass_add_workouts(workouts)
-    assert wd.get_individual_workout(4, 5, 6) == workouts[2]
+    assert wd.get_individual_workout(4, 5, 6) == workouts[5]
     assert wd.get_workout_type(4, 5, 6) == "Progression"
-    assert "Progression" == wd.get_workout_type(workouts[2].get_stim(), workouts[2].get_rpe(), workouts[2].get_distance())
+    assert "Progression" == wd.get_workout_type(workouts[5].get_stim(), workouts[5].get_rpe(), workouts[5].get_distance())
     assert wd.get_workout_type_coordinates(4, 5, 6) == wd.create_trio(4, 6, 6)
