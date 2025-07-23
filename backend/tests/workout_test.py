de0bb58c5b7437a0da36bb78ebe556f847f2f4aa
owen1k2 <owenhblock@gmail.com>
2025-07-23 12:58:36 -0500

    testing

--- backend/tests/workout_test.py
+++ backend/tests/workout_test.py
@@ -11,4 +11,4 @@ def test_workout_database():
     assert wd.get_individual_workout(4, 5, 6) == workouts[2]
     assert wd.get_workout_type(4, 5, 6) == "Progression"
     assert "Progression" == wd.get_workout_type(workouts[2].get_stim(), workouts[2].get_rpe(), workouts[2].get_distance())
-    assert wd.get_workout_type_coordinates(4, 5, 6) == wd.create_trio(4, 5, 6)
+    assert wd.get_workout_type_coordinates(4, 5, 6) == wd.create_trio(4, 6, 6)
